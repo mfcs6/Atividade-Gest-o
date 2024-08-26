5711195cf947c3ac519f216f731a4fcd2a023ef5
Grupo1 <mfcs6@aluno.ifal.edu.br>
2024-08-26 10:42:11 -0300

    Não sei se ta certo kkkk
    Junção de todos os códigos

--- Main.java
+++ Main.java
@@ -1,5 +1,16 @@
 public class Main {
     public static void main(String args[]){
 
+        Banco meu_banco = new Banco();
+        Contabancaria conta1 = new Contabancaria();
+        Contabancaria conta2 = new Contabancaria();
+
+        meu_banco = Banco("Banco Central");
+        conta1 = meu_banco.criar_conta("Ana Carla",    500)
+        conta2 = meu_banco.criar_conta("João Silva", 500);
+        conta1.vsaldo();
+        conta1.vsaldo();
+        conta2.vsaldo();    
+
     }
 }
