7d3e1de1853248b687f6ef700b43fee0ad77154d
Alisson <161031456+Alisson977@users.noreply.github.com>
2024-08-26 10:21:49 -0300

    ultima tentativa

--- Banco.java
+++ Banco.java
@@ -1,7 +1,24 @@
 class Banco extends Contabancaria {
     String Banco;
+    String conta;
+
     public void criar_conta(String titular, int Saldo_inicial){
-        
+        super(titular,Saldo_inicial);
+        this.titular = titular;
+        this.Saldo_inicial = Saldo_inicial;
+    }
+
+    public void encontrar_conta(int numero){
+        super(numero);
+        return titular;
+    }
+
+    public void tranferir(int valor,String titular1, String titular2){
+        super(conta)
+
+        if(conta == titular1 && conta == titular2){
+            
+        }
     }
 
 }
