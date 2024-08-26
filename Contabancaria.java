8b7b82b03b7466102c83673678ff958041d579c2
cibellefss <cfss4@aluno.ifal.edu.br>
2024-08-26 10:33:36 -0300

    commit
    teste0075

--- Contabancaria.java
+++ Contabancaria.java
@@ -1,3 +1,26 @@
 class Contabancaria extends Banco {
+int numconta;
+String nome;
+double saldo;
+double saque;
+
+public void depositar(double valord){
+    valord = saldo;
+
+}public void sacar (double valors){
     
+    if (saldo >= saque){
+        System.out.println(" Saldo suficiente para saque.");
+        valors = saldo - saque;
+    }
+
+    else{
+        System.out.println(" Saldo insuficiente.");
+    }
+}
+
+public double vsaldo(){
+    System.out.println(" Seu saldo é: " + vsaldo());
+    return saldo;
+}
 }
