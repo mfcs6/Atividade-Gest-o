a86c9416619195f63809e4f6c87742fd75cbc227
Alisson <161031456+Alisson977@users.noreply.github.com>
2024-08-26 09:58:52 -0300

    primeiras modificações

--- Banco.java
+++ Banco.java
@@ -1,3 +1,7 @@
-public class Banco {
-    
+class Banco extends Contabancaria {
+    String Banco;
+    public void criar_conta(String titular, int Saldo_inicial){
+        
+    }
+
 }
--- Contabancaria.java
+++ Contabancaria.java
@@ -1,3 +1,3 @@
-class Contabancaria extends Banco {
+public class Contabancaria {
     
 }
